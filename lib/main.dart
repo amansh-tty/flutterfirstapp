68f92ee5215f47987f43a8b5c5a67d02b0068c93
amansh-tty <amanshetty300@gmail.com>
2023-06-24 23:20:40 +0530

    updated

--- lib/main.dart
+++ lib/main.dart
@@ -6,43 +6,86 @@ class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
   @override
-  MyAppState createState() => MyAppState();
+  // ignore: library_private_types_in_public_api
+  _MyAppState createState() => _MyAppState();
 }
 
-class MyAppState extends State<MyApp> {
-  double _numberFrom = 0;
+class _MyAppState extends State<MyApp> {
+  final TextStyle inputStyle = const TextStyle(
+    fontSize: 18,
+    color: Colors.black87,
+  );
+
+  final TextStyle labelStyle = const TextStyle(
+    fontSize: 20,
+    color: Colors.black,
+  );
+  String? _startMeasure;
+  String? _convertedMeasure;
+  double? _numberForm;
+  String? _resultMessage;
 
   @override
   void initState() {
-    _numberFrom = 0;
+    _numberForm = 0;
     super.initState();
   }
 
-  String? _convertedMeasure;
-  String? _startMeasures;
+  final List<String> _measures = [
+    'meters',
+    'kilometers',
+    'grams',
+    'kilograms',
+    'feet',
+    'miles',
+    'pounds (lbs)',
+    'ounces',
+  ];
+  final Map<String, int> _measuresMap = {
+    'meters': 0,
+    'kilometers': 1,
+    'grams': 2,
+    'kilograms': 3,
+    'feet': 4,
+    'miles': 5,
+    'pounds (lbs)': 6,
+    'ounces': 7,
+  };
+
+  final dynamic _formulas = {
+    '0': [1, 0.001, 0, 0, 3.28084, 0.000621371, 0, 0],
+    '1': [1000, 1, 0, 0, 3280.84, 0.621371, 0, 0],
+    '2': [0, 0, 1, 0.0001, 0, 0, 0.00220462, 0.035274],
+    '3': [0, 0, 1000, 1, 0, 0, 2.20462, 35.274],
+    '4': [0.3048, 0.0003048, 0, 0, 1, 0.000189394, 0, 0],
+    '5': [1609.34, 1.60934, 0, 0, 5280, 1, 0, 0],
+    '6': [0, 0, 453.592, 0.453592, 0, 0, 1, 16],
+    '7': [0, 0, 28.3495, 0.0283495, 3.28084, 0, 0.0625, 1],
+  };
+
+  void convert(double value, String from, String to) {
+    var nFrom = _measuresMap[from];
+    var nTo = _measuresMap[to];
+    var multiplier = _formulas[nFrom.toString()][nTo];
+    var result = value * multiplier;
+    if (result == 0) {
+      _resultMessage = 'This conversion cannot be performed';
+    } else {
+      _resultMessage =
+          '${_numberForm.toString()} $_startMeasure are ${result.toString()} $_convertedMeasure';
+    }
+    setState(() {
+      _resultMessage = _resultMessage;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    final TextStyle inputStyle = TextStyle(
-      fontSize: 24,
-      color: Colors.blue[900],
-    );
-    final TextStyle labelStyle = TextStyle(
-      fontSize: 24,
-      color: Colors.grey[700],
-    );
-    final List<String> measures = [
-      'meters',
-      'kilometers',
-      'grams',
-      'kilograms',
-      'feet',
-      'miles',
-      'pounds (lbs)',
-      'ounces',
-    ];
     return MaterialApp(
-      title: 'Measure Converter',
+      debugShowCheckedModeBanner: false,
+      title: 'Measures Converter',
       home: Scaffold(
+        resizeToAvoidBottomInset: true,
         appBar: AppBar(
           title: const Text('Measures Converter'),
         ),
@@ -50,105 +93,103 @@ class MyAppState extends State<MyApp> {
           padding: const EdgeInsets.symmetric(horizontal: 20),
           child: Column(
             children: [
-              Spacer(),
-              Text(
-                'Value',
-                style: labelStyle,
-              ),
-              Spacer(),
+              const Spacer(),
               TextField(
                 style: inputStyle,
-                decoration: InputDecoration(
-                  hintText: "Please insert the measure to be converted",
+                decoration: const InputDecoration(
+                  hintText: "Please enter the value",
                 ),
                 onChanged: (text) {
                   var rv = double.tryParse(text);
                   if (rv != null) {
                     setState(() {
-                      _numberFrom = rv;
+                      _numberForm = rv;
                     });
                   }
                 },
               ),
-              // DropdownButton(
-              //   isExpanded: true,
-              //   style: inputStyle,
-              //   items: measures.map((String value) {
-              //     return DropdownMenuItem<String>(
-              //       value: value,
-              //       child: Text(
-              //         value,
-              //         style: inputStyle,
-              //       ),
-              //     );
-              //   }).toList(),
-              //   onChanged: (value) {
-              //     setState(() {
-              //       _convertedMeasure = value;
-              //     });
-              //   },
-              //   value: _convertedMeasure,
-              // ),
-              Spacer(),
-              Spacer(
-                flex: 2,
-              ),
-
-              Spacer(),
-              Text(
-                'From',
-                style: labelStyle,
-              ),
-              DropdownButton(
-                isExpanded: true,
-                items: measures.map((String value) {
-                  return DropdownMenuItem<String>(
-                    value: value,
-                    child: Text(value),
-                  );
-                }).toList(),
-                onChanged: (value) {
-                  setState(() {
-                    _startMeasures = value;
-                  });
-                },
-                value: _startMeasures,
-              ),
-              Spacer(),
-              Spacer(),
-              Text(
-                'To',
-                style: labelStyle,
-              ),
-              Spacer(),
-              DropdownButton(
-                isExpanded: true,
-                style: inputStyle,
-                items: measures.map((String value) {
-                  return DropdownMenuItem<String>(
-                    value: value,
-                    child: Text(
-                      value,
+              const Spacer(),
+              //Text((_numberForm==null)? '' : _numberForm.toString())
+              Row(
+                children: [
+                  DropdownButton(
+                    style: inputStyle,
+                    hint: Text(
+                      "Unit",
                       style: inputStyle,
                     ),
-                  );
-                }).toList(),
-                onChanged: (value) {
-                  setState(() {
-                    _convertedMeasure = value;
-                  });
-                },
-                value: _convertedMeasure,
-              ),
-              Spacer(),
-              Spacer(),
-              ElevatedButton(
-                child: Text('Convert', style: inputStyle),
-                onPressed: () => true,
+                    items: _measures.map((String value) {
+                      return DropdownMenuItem<String>(
+                        value: value,
+                        child: Text(value),
+                      );
+                    }).toList(),
+                    onChanged: (value) {
+                      setState(() {
+                        _startMeasure = value;
+                      });
+                    },
+                    value: _startMeasure,
+                  ),
+                  const Spacer(),
+                  Icon(
+                    Icons.arrow_forward,
+                    color: Colors.blue[600],
+                    size: 24.0,
+                    semanticLabel: 'Text to announce in accessibility modes',
+                  ),
+                  const Spacer(),
+                  DropdownButton(
+                    hint: Text(
+                      "Unit",
+                      style: inputStyle,
+                    ),
+                    style: inputStyle,
+                    items: _measures.map((String value) {
+                      return DropdownMenuItem<String>(
+                        value: value,
+                        child: Text(
+                          value,
+                          style: inputStyle,
+                        ),
+                      );
+                    }).toList(),
+                    onChanged: (value) {
+                      setState(() {
+                        _convertedMeasure = value;
+                      });
+                    },
+                    value: _convertedMeasure,
+                  ),
+                ],
               ),
 
-              Text((_numberFrom == null) ? '' : _numberFrom.toString(),
+              const Spacer(
+                flex: 1,
+              ),
+              ElevatedButton(
+                // color: Colors.blue,
+                child: const Text('Convert',
+                    style: TextStyle(color: Colors.white)),
+                onPressed: () {
+                  if (_startMeasure!.isEmpty ||
+                      _convertedMeasure!.isEmpty ||
+                      _numberForm == 0) {
+                    return;
+                  } else {
+                    convert(_numberForm!, _startMeasure!, _convertedMeasure!);
+                  }
+                },
+              ),
+
+              const Spacer(
+                flex: 1,
+              ),
+              Text((_resultMessage == null) ? '' : _resultMessage.toString(),
                   style: labelStyle),
+              const Spacer(
+                flex: 8,
+              ),
             ],
           ),
         ),
